66be1c64ed86af9439aaccc19f6b5089ccc943b8
Daniel Berger <djberg96@gmail.com>
2014-10-12 09:08:22 -0600

    No long explicitly require rubygems.

--- test/test_interface.rb
+++ test/test_interface.rb
@@ -3,10 +3,7 @@
 #
 # Test suite for the Interface module.
 #####################################################
-require 'rubygems'
-gem 'test-unit'
-
-require 'test/unit'
+require 'test-unit'
 require 'interface'
 
 class TC_Interface < Test::Unit::TestCase
@@ -20,7 +17,7 @@ class TC_Interface < Test::Unit::TestCase
       required_methods :gamma
       unrequired_methods :alpha
     }
-      
+
     # Workaround for 1.9.x
     @@alpha_interface = alpha_interface
     @@gamma_interface = gamma_interface
@@ -45,7 +42,7 @@ class TC_Interface < Test::Unit::TestCase
   def test_version
     assert_equal('1.0.3', Interface::VERSION)
   end
-	
+
   def test_interface_requirements_not_met
     assert_raise(Interface::MethodMissing){ A.extend(@@alpha_interface) }
     assert_raise(Interface::MethodMissing){ A.new.extend(@@alpha_interface) }
@@ -55,7 +52,7 @@ class TC_Interface < Test::Unit::TestCase
     assert_raise(Interface::MethodMissing){ B.extend(@@gamma_interface) }
     assert_raise(Interface::MethodMissing){ B.new.extend(@@gamma_interface) }
   end
-	
+
   def test_alpha_interface_requirements_met
     assert_nothing_raised{ B.new.extend(@@alpha_interface) }
   end
